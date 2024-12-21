887208a7b76361c2e7219bf02d98f4a3fc7068e8
Nick Sia <fasterthoughts@gmail.com>
2024-12-21 13:18:06 +0800

    start rep

--- pg-dig-server/src/main.rs
+++ pg-dig-server/src/main.rs
@@ -2,15 +2,23 @@
 #![allow(non_upper_case_globals)]
 
 use pg_dig_server::libpq::*;
-use pg_dig_server::print_status;
-use std::ffi::CString;
+use pg_dig_server::{exec, print_status, to_cstr};
 
+const LOCAL_CONNECTION_STRING: &str = "host=localhost user=postgres dbname=postgres password=postgres replication=database";
 fn main() {
-
     unsafe {
-        let connection_str = CString::new("host=localhost user=postgres dbname=postgres password=postgres").unwrap();
-        println!("{}", connection_str.to_str().unwrap());
-        let conn = PQconnectdb(connection_str.as_ptr());
-        print_status(conn);
+        let conn = connect(LOCAL_CONNECTION_STRING);
+        let result = exec(conn,"START_REPLICATION SLOT physical PHYSICAL 0/0");
+        PQfinish(conn);
     }
 }
+
+unsafe fn connect(conn_string: &str) -> *mut pg_conn {
+    let conn_string = to_cstr(LOCAL_CONNECTION_STRING);
+    println!("connecting: {}", conn_string.to_str().unwrap());
+    let conn = PQconnectdb(conn_string.as_ptr());
+    print_status(conn);
+
+    conn
+}
+
